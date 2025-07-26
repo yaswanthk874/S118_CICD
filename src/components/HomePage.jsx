f9bef2b5594954b6bfd8b4e498484abb83b666bf
MSVVSSAINADH <macharlasainadh2506@gmail.com>
2025-07-26 09:34:53 +0530

    ui changes

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
     <div className="container">
       {/* Header */}
       <div className="header">
-        <h1>E-Commerce</h1>
+        <h1>Yaswanth's Smart store</h1>
         <div className="header-right">
           {isAuthenticated ? (
             <div className="profile-menu">
